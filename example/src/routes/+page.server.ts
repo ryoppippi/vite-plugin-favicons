c7b896a17f805aa03c05807edfcd8bfc104ca393
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-27 00:22:44 +0100

    feat(example): add html-format for formatting HTML content
    - Added `html-format` dependency to `package.json`.
    - Updated `+page.server.ts` to use `html-format` for formatting HTML content.
    - Updated `pnpm-lock.yaml` to include `html-format` dependency.

--- example/src/routes/+page.server.ts
+++ example/src/routes/+page.server.ts
@@ -1,14 +1,16 @@
 import favicons from 'virtual:favicons'
 import { codeToHtml } from 'shiki'
+import format from "html-format";
 
 export const load = async ()=>{
+  const html = format(`<head>\n${favicons}\n</head>`);
   const faviconsHtml = await codeToHtml(
-    `<head>\n${favicons}\n</head>`,
+    html,
     {
       lang: 'html',
       theme: 'vitesse-dark'
     }
-  )
+  );
 
   return { faviconsHtml }
 }
