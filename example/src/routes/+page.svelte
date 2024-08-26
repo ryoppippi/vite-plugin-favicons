1e54e90a6fffd72585facf85dbcac4d95eb74a4d
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-26 16:08:45 +0100

    feat: show badge on example page
    - Added `svelte-exmarkdown` dependency to `package.json`.
    - Updated `+page.svelte` to use `svelte-exmarkdown` for rendering
    markdown.
    - Modified `pnpm-lock.yaml` to include `svelte-exmarkdown` and its
    dependencies.
    - Updated styles to remove text decoration from links.

--- example/src/routes/+page.svelte
+++ example/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 import favicons from 'virtual:favicons'
+import Markdown from 'svelte-exmarkdown';
 
 const { data } = $props()
 </script>
@@ -8,8 +9,22 @@ const { data } = $props()
   {@html favicons}
 </svelte:head>
 
-<h1> Generated Favicon Meta Data </h1>
-<h2> Also check the actual header using DevTools </h2>
+<!-- <h1>vite-plugin-favicon </h1> -->
+<!---->
+<!-- <h2> Generated Favicon Meta Data </h2> -->
+<!-- <h2> Also check the actual header using DevTools </h2> -->
+
+<Markdown
+  md={`
+# [vite-plugin-favicon](https://github.com/ryoppippi/vite-plugin-favicons)
+
+[![npm version](https://img.shields.io/npm/v/vite-plugin-favicons?color=yellow)](https://npmjs.com/package/vite-plugin-favicons)
+[![npm downloads](https://img.shields.io/npm/dm/vite-plugin-favicons?color=yellow)](https://npmjs.com/package/vite-plugin-favicons)
+
+## Generated Favicon Meta Data
+## Also check the actual header using DevTools
+`}
+/>
 
 <div>
   {@html data.faviconsHtml}
@@ -18,6 +33,12 @@ const { data } = $props()
 <style>
 div {
   font-size: 1rem;
+}
+
+:global{
+  a{
+    text-decoration: none; 
+  }
 }
 </style>
 
