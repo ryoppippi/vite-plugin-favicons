3182ca9f01b71beaae6915e338b8a76a6b2d2e58
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-09-20 19:55:12 +0100

    feat: use markdown-it

--- example/src/routes/+page.svelte
+++ example/src/routes/+page.svelte
@@ -1,28 +1,9 @@
 <script>
-  import Markdown from 'svelte-exmarkdown';
-
   const { data } = $props()
 </script>
 
-<!-- <h1>vite-plugin-favicon </h1> -->
-<!---->
-<!-- <h2> Generated Favicon Meta Data </h2> -->
-<!-- <h2> Also check the actual header using DevTools </h2> -->
-
-<Markdown
-  md={`
-# [vite-plugin-favicon](https://github.com/ryoppippi/vite-plugin-favicons)
-
-[![npm version](https://img.shields.io/npm/v/vite-plugin-favicons?color=yellow)](https://npmjs.com/package/vite-plugin-favicons)
-[![npm downloads](https://img.shields.io/npm/dm/vite-plugin-favicons?color=yellow)](https://npmjs.com/package/vite-plugin-favicons)
-
-## Generated Favicon Meta Data
-## Also check the actual header using DevTools
-`}
-/>
-
 <div>
-  {@html data.faviconsHtml}
+  {@html data.markdown}
 </div>
 
 <style>
